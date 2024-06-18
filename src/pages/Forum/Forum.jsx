5341a91da05c1467c2f7560678a925a348077267
Joaop-Filho <fjoao3756@gmail.com>
2024-06-18 16:07:07 -0300

    corrigindo o forum

--- src/pages/Forum/Forum.jsx
+++ src/pages/Forum/Forum.jsx
@@ -2,11 +2,12 @@ import './Forum.css';
 import React from "react";
 import Conversa from "../../components/conversation/Conversa";
 import { Widget } from "../../components/Widget";
+import Header from '../../components/header/header';
 
 function Forum() {
   return (
     <>
-    <Header />
+    <Header className='header-forum' />
     <div className='body-forum'>
       {<Widget />}
       <Conversa></Conversa>
